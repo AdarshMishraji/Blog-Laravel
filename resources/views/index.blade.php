85b03d5cb5bab44965fe404e641be6f625e2c488
AdarshMishraji <adarshmishra969@gmail.com>
2022-04-02 21:58:09 +0530

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+    <div class='background-image grid grid-cols-1 m-auto'>
+        <div class='flex text-gray-100 pt-10'>
+            <div class='m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center'>
+                <h1 class='sm:text-white text-5xl uppercase font-bold transition-shadow-md pb-14'>
+                    Do you want to be a developer?
+                </h1>
+                <a href='/blog' class='text-center bg-gray-50 to-gray-700 py-2 px-4 font-bold text-xl uppercase text-black'>
+                    Read More
+                </a>
+            </div>
+        </div>
+    </div>
+@endsection
