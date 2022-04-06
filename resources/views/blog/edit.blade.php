1f35f2aeca1f38d5cc57a36153acdbc29d3a2213
AdarshMishraji <adarshmishra969@gmail.com>
2022-04-06 21:22:57 +0530

    revamped blog related pages

--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.app')
 @section('content')
-    <div class="w-4/5 m-auto text-left">
-        <div class="py-15">
-            <h1 class="text-6xl ">
-                Update Post
-            </h1>
+    <div class="from-indigo-400 via-indigo-600 bg-gradient-to-bl  to-indigo-700 clearfix">
+        <div class="w-4/5 m-auto text-left">
+            <div class="pt-20">
+                <h1 class="text-6xl font-bold text-white">
+                    Update Post
+                </h1>
+            </div>
         </div>
-    </div>
 
-    <div class="w-4/5 m-auto pt-20">
-        <form action="/blog/{{ $post->slug }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
-            <input type="text" name="title" value={{ $post->title }}
-                class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+        <div class="w-4/5 mys-10 mx-auto pt-20">
+            <form action="/blog/{{ $post->slug }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <input type="text" name="title" value={{ $post->title }}
+                    class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
 
-            <textarea name="description" class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none">{{ $post->description }}
+                <textarea name="description" class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none">{{ $post->description }}
             </textarea>
 
-            <button type="submit"
-                class="uppercase mt-15 bg-blue-800 text-white text-lg font-extrabold py-4 px-8 rounded-3xl">Submit
-                Post</button>
-        </form>
+                <button type="submit"
+                    class="uppercase mt-15 bg-blue-800 text-white text-lg font-extrabold py-4 px-8 rounded-3xl">Submit
+                    Post</button>
+            </form>
+        </div>
     </div>
 @endsection
--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,22 +1,25 @@
 @extends('layouts.app')
 @section('content')
-    <div class="w-4/5 m-auto text-left">
-        <div class="py-15">
-            <h1 class="text-6xl ">
-                {{ $post->title }}
-            </h1>
+    <div class="from-indigo-400 via-indigo-600 bg-gradient-to-bl  to-indigo-700 clearfix">
+        <div class="w-4/5 m-auto text-left">
+            <div class="pt-20">
+                <h1 class="text-6xl  text-white font-bold">
+                    {{ $post->title }}
+                </h1>
+            </div>
         </div>
-    </div>
-    <div class="w-4/5 m-auto pt-10 bg-white rounded-2xl shadow-2xl px-10">
-        <div>
-            <img src="{{ asset('images/' . $post->image_path) }}" alt="" width="700">
+        <div class="w-4/5 my-10 mx-auto pt-10 bg-white rounded-2xl shadow-2xl px-10">
+            <div class="flex justify-center">
+                <img src="{{ asset('images/' . $post->image_path) }}" alt="" width="500px" height="500px"
+                    class="rounded-3xl object-cover">
+            </div>
+            <span class="text-gray-500">
+                By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>,Created on
+                {{ date('jS M Y', strtotime($post->updated_at)) }}
+            </span>
+            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                {{ $post->description }}
+            </p>
         </div>
-        <span class="text-gray-500">
-            By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>,Created on
-            {{ date('jS M Y', strtotime($post->updated_at)) }}
-        </span>
-        <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-            {{ $post->description }}
-        </p>
     </div>
 @endsection
